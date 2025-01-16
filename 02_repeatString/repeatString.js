53c7c2cc342df191926d02e49244390c7265e9b1
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-16 16:07:44 +0000

    Create for loop and add string to var word x times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    word = '';
+    for (let i = 0; i < number; i++) {
+        word += string;
+    }
 
+    return word;
 };
 
 // Do not edit below this line
