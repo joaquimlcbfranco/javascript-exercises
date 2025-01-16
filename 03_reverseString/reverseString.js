2189b60eeb774e4b90cbad23dd6e465f995446c2
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-16 16:37:07 +0000

    Set blank word eq. to substr of string from end to start

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    word = '';
+    for (let i = string.length; i >= 0; i--) {
+        word += string.substr(i, 1);
+    }
 
+    return word;
 };
 
 // Do not edit below this line
