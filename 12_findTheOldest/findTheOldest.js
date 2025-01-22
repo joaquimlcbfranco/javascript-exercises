b79780695a82fe21888fa98cfe182beb949043a9
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-22 14:58:52 +0000

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.reduce((prevPerson, currentPerson) => {
+        if (!Object.keys(prevPerson).length) {
+            prevPerson = currentPerson;
+        }
+        else {
+            if ((!Object.keys(currentPerson).includes('yearOfDeath') ? new Date().getFullYear() : currentPerson.yearOfDeath) - currentPerson.yearOfBirth >
+                (!Object.keys(prevPerson).includes('yearOfDeath') ? new Date().getFullYear() : prevPerson.yearOfDeath) - prevPerson.yearOfBirth) 
+            {
+                prevPerson = currentPerson;
+            }
+        }
+        return prevPerson;
+    }, {});
+    return oldest;
 };
 
 // Do not edit below this line
