077b45e4671627b1e27429730096c1fd130708e3
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-22 11:28:41 +0000

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    
+    const word = string
+                    .toLowerCase()
+                    .split('')
+                    .filter(char => alphanumerical.includes(char))
+                    .join('')
 
+    return word.split('').reverse().join('') === word ? true : false;
 };
 
 // Do not edit below this line
