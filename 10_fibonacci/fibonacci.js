8d1852751ef733cda2d7509435b42d16c77641ae
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-22 12:00:50 +0000

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) {
+        return 'OOPS';
+    }
 
-};
+    let arr = [0];
+    index = +index;
+
+    for (arr.length; arr.length - 1 !== index;) {
+            if (arr.length === 1) {
+                arr.push(1);
+            }
+            else {
+                newElem = arr[arr.length - 1] + arr[arr.length - 2];
+                arr.push(newElem);
+            }
+        }
+    return arr[index];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
