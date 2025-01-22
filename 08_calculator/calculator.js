2e36774e644a80d97bb028c66b95072b82b1703d
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-22 11:14:39 +0000

    Complete all calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, number) => total * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	total = 1;
+  while (number != 0) {
+    total *= number
+    number--;
+  }
+  return total;
 };
 
 // Do not edit below this line
