e553f128ba351b4b62dba48c3113c5ee284a36e7
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-22 12:25:56 +0000

    Complete getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    const newArray = obj.reduce((prevRow, currentRow) => {
+        prevRow.push(currentRow.title);
+        return prevRow;
+    }, [])
+    return newArray;
 };
 
 // Do not edit below this line
